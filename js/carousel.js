858edd8c79fe3bec5d96aa0387f350d99fa29219
ZCLMK <zacharie.NTC@gmail.com>
2018-03-22 20:35:48 +0100

    créé fonction pour calcul de la largeur, début translate

--- js/carousel.js
+++ js/carousel.js
@@ -14,21 +14,20 @@ class Carousel {
             slidesVisible: 1
         }, options)
         // this.children = [].slice.call(element.children)
-        this.children = [...element.children]
-        console.log(this.children)
-        // let ratio = this.children.length/this.options.slidesVisible
-        let root = this.createDivWithClass('carousel')
-        let container = this.createDivWithClass('carousel-container')
-        // container.style.width = (ratio * 100) + "%"
-        console.log(root, container)
-        element.appendChild(root)
-        root.appendChild(container)
-        this.children.forEach((child)=>{
+        let enfants = [...element.children]
+        this.currentItem = 0
+        this.root = this.createDivWithClass('carousel')
+        this.container = this.createDivWithClass('carousel-container')
+        element.appendChild(this.root)
+        this.root.appendChild(this.container)
+        this.items = enfants.map((child)=>{
           let item = document.createElement('div')
-          item.classList.add('container-item')
+          item.classList.add('carousel-item')
           item.appendChild(child)
-          container.appendChild(item)
+          this.container.appendChild(item)
+          return item 
         })
+        this.setStyle()
         // !: si on utilise un fonction preES6, this n'est plus la classe mais l'élément
     }
         /**
@@ -40,6 +39,38 @@ class Carousel {
             maDiv.setAttribute('class', className)
             return maDiv
             }
+/**
+ * Défini la largeur des images du carousel
+ */
+        setStyle(){
+            let ratio = this.items.length/this.options.slidesVisible
+            this.container.style.width = (ratio * 100) + "%" 
+            this.items.forEach(item =>  item.style.width = (100/this.options.slidesVisible)/ratio + "%")
+        }
+
+        createNavigation(){
+            let nextButton = this.createDivWithClass('carousel-next')
+            let prevButton = this.createDivWithClass('carousel-prev')
+            this.root.appendChild(nextButton)
+            this.root.appendChild(prevButton)
+            nextButton.addEventListener('click', this.next.bind(this))
+            prevButton.addEventListener('click', this.prev.bind(this))
+            // on utilise "bind(this)" pour que dans this.next & this.prev, this => la classe, pas à la 
+            //fonction createNavigation
+        }
+
+        prev(){
+            this.goToItem(this.currentItem + this.options.slidesToScroll)
+        }
+
+        next(){
+            this.goToItem(this.currentItem - this.options.slidesToScroll)
+        }
+        goToItem(index){
+            let translateX = index * 100 / items.length
+            this.container.style.transform = 'translate3D('+ translateX +'%,0,0,)'
+            this.currentItem = index
+        }
     }
 
 document.addEventListener('DOMContentLoaded', function(){
